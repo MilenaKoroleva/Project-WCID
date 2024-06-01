d240bf26d6ca3e9088d2bb81192406e4649fe9d6
user <user@mail.com>
2024-06-01 07:09:32 +0300

    БД

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,10 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    titel=models.CharField(max_length=150)
+    description=models.TextField(blank=True)
+
+    class Meta:
+        db_table="notes"
+
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    titel=models.CharField(max_length=150)
+    important=models.BooleanField(default=False)
+    urgently=models.BooleanField(default=False)
+    todo_date=models.DateField(blank=True, null=True)
+    description=models.TextField(blank=True)
+
+    class Meta:
+        db_table="task_list"
+
